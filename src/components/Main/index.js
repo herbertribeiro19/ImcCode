d675f91bbbd3b8dfe24f1910f96ffd2ec9837480
Herbert Ribeiro <hebert11223@gmail.com>
2024-04-27 16:27:05 -0300

    updateBG

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,11 @@
-import React from "react"
-import { View, Text, StyleSheet } from 'react-native';
-import Form from "../Form/"
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Form from "../Form/";
 
 export default function Main() {
-    return (
-        <View>
-            <Form />
-        </View>
-    );
+  return (
+    <View>
+      <Form />
+    </View>
+  );
 }
